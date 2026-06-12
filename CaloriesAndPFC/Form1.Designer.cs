5356a86f8f9d7f34f21c830b421ffd3c8df00347
Дмитрий <dimacoolda@gmail.com>
2026-06-12 20:01:20 +0300

    Final

--- CaloriesAndPFC/Form1.Designer.cs
+++ CaloriesAndPFC/Form1.Designer.cs
@@ -29,187 +29,221 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Gender = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.Age = new System.Windows.Forms.NumericUpDown();
+            this.Height = new System.Windows.Forms.NumericUpDown();
+            this.Weight = new System.Windows.Forms.NumericUpDown();
+            this.Activity = new System.Windows.Forms.ComboBox();
+            this.Goal = new System.Windows.Forms.ComboBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.SaveTxt = new System.Windows.Forms.Button();
+            this.SaveDb = new System.Windows.Forms.Button();
+            this.ShowHistory = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Height)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Weight)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(132, 38);
+            this.label1.Location = new System.Drawing.Point(95, 120);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Пол:";
             // 
-            // comboBox1
+            // Gender
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(135, 117);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
+            this.Gender.FormattingEnabled = true;
+            this.Gender.Location = new System.Drawing.Point(128, 117);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(121, 21);
+            this.Gender.TabIndex = 1;
+            this.Gender.Text = "Мужской, Женский";
+            this.Gender.SelectedIndexChanged += new System.EventHandler(this.Gender_SelectedIndexChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(182, 38);
+            this.label2.Location = new System.Drawing.Point(252, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(52, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Возраст:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 38);
+            this.label3.Location = new System.Drawing.Point(250, 146);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.label3.Text = "Рост(см):";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(303, 38);
+            this.label4.Location = new System.Drawing.Point(255, 173);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(49, 13);
             this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.label4.Text = "Вес (кг):";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(362, 38);
+            this.label5.Location = new System.Drawing.Point(10, 146);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(115, 13);
             this.label5.TabIndex = 5;
-            this.label5.Text = "label5";
+            this.label5.Text = "Уровень активности:";
             // 
-            // numericUpDown1
+            // Age
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(306, 117);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 6;
+            this.Age.Location = new System.Drawing.Point(306, 117);
+            this.Age.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(120, 20);
+            this.Age.TabIndex = 6;
             // 
-            // numericUpDown2
+            // Height
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(306, 143);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown2.TabIndex = 7;
+            this.Height.Location = new System.Drawing.Point(306, 143);
+            this.Height.Maximum = new decimal(new int[] {
+            230,
+            0,
+            0,
+            0});
+            this.Height.Name = "Height";
+            this.Height.Size = new System.Drawing.Size(120, 20);
+            this.Height.TabIndex = 7;
             // 
-            // numericUpDown3
+            // Weight
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(306, 169);
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown3.TabIndex = 8;
+            this.Weight.Location = new System.Drawing.Point(306, 169);
+            this.Weight.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.Weight.Name = "Weight";
+            this.Weight.Size = new System.Drawing.Size(120, 20);
+            this.Weight.TabIndex = 8;
             // 
-            // comboBox2
+            // Activity
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(135, 144);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 9;
+            this.Activity.FormattingEnabled = true;
+            this.Activity.Location = new System.Drawing.Point(128, 143);
+            this.Activity.Name = "Activity";
+            this.Activity.Size = new System.Drawing.Size(121, 21);
+            this.Activity.TabIndex = 9;
+            this.Activity.Text = "Минимальная, Низкая, Средняя, Высокая, Экстремальная";
             // 
-            // comboBox3
+            // Goal
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(135, 171);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 21);
-            this.comboBox3.TabIndex = 10;
+            this.Goal.FormattingEnabled = true;
+            this.Goal.Location = new System.Drawing.Point(128, 170);
+            this.Goal.Name = "Goal";
+            this.Goal.Size = new System.Drawing.Size(121, 21);
+            this.Goal.TabIndex = 10;
+            this.Goal.Text = "Набор мышечной массы, Похудение, Поддержание формы";
             // 
-            // button1
+            // Calculate
             // 
-            this.button1.Location = new System.Drawing.Point(449, 117);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Calculate.Location = new System.Drawing.Point(450, 117);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(110, 23);
+            this.Calculate.TabIndex = 11;
+            this.Calculate.Text = "Рассчитать";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
             // 
-            // button2
+            // SaveTxt
             // 
-            this.button2.Location = new System.Drawing.Point(449, 140);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 12;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.SaveTxt.Location = new System.Drawing.Point(450, 140);
+            this.SaveTxt.Name = "SaveTxt";
+            this.SaveTxt.Size = new System.Drawing.Size(111, 23);
+            this.SaveTxt.TabIndex = 12;
+            this.SaveTxt.Text = "Сохранить в TXT";
+            this.SaveTxt.UseVisualStyleBackColor = true;
+            this.SaveTxt.Click += new System.EventHandler(this.SaveTxt_Click);
             // 
-            // button3
+            // SaveDb
             // 
-            this.button3.Location = new System.Drawing.Point(449, 166);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 13;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.SaveDb.Location = new System.Drawing.Point(450, 166);
+            this.SaveDb.Name = "SaveDb";
+            this.SaveDb.Size = new System.Drawing.Size(110, 23);
+            this.SaveDb.TabIndex = 13;
+            this.SaveDb.Text = "Сохранить в БД";
+            this.SaveDb.UseVisualStyleBackColor = true;
+            this.SaveDb.Click += new System.EventHandler(this.SaveDb_Click);
             // 
-            // button4
+            // ShowHistory
             // 
-            this.button4.Location = new System.Drawing.Point(449, 192);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 14;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.ShowHistory.Location = new System.Drawing.Point(451, 192);
+            this.ShowHistory.Name = "ShowHistory";
+            this.ShowHistory.Size = new System.Drawing.Size(110, 23);
+            this.ShowHistory.TabIndex = 14;
+            this.ShowHistory.Text = "Показать историю";
+            this.ShowHistory.UseVisualStyleBackColor = true;
+            this.ShowHistory.Click += new System.EventHandler(this.ShowHistory_Click);
             // 
-            // richTextBox1
+            // Result
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(565, 119);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
-            this.richTextBox1.TabIndex = 15;
-            this.richTextBox1.Text = "";
+            this.Result.Location = new System.Drawing.Point(566, 119);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(223, 96);
+            this.Result.TabIndex = 15;
+            this.Result.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Цель:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.numericUpDown3);
-            this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.ShowHistory);
+            this.Controls.Add(this.SaveDb);
+            this.Controls.Add(this.SaveTxt);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.Goal);
+            this.Controls.Add(this.Activity);
+            this.Controls.Add(this.Weight);
+            this.Controls.Add(this.Height);
+            this.Controls.Add(this.Age);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Gender);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Height)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Weight)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -218,21 +252,22 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Gender;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
-        private System.Windows.Forms.NumericUpDown numericUpDown3;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown Age;
+        private System.Windows.Forms.NumericUpDown Height;
+        private System.Windows.Forms.NumericUpDown Weight;
+        private System.Windows.Forms.ComboBox Activity;
+        private System.Windows.Forms.ComboBox Goal;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button SaveTxt;
+        private System.Windows.Forms.Button SaveDb;
+        private System.Windows.Forms.Button ShowHistory;
+        private System.Windows.Forms.RichTextBox Result;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- CaloriesAndPFC/Form1.cs
+++ CaloriesAndPFC/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,204 @@ namespace CaloriesAndPFC
         public Form1()
         {
             InitializeComponent();
+            Gender.Items.AddRange(new string[] { "Мужской", "Женский" });
+            Gender.SelectedIndex = 0;
+
+            Activity.Items.AddRange(new string[] { "Минимальная", "Низкая", "Средняя", "Высокая", "Экстремальная" });
+            Activity.SelectedIndex = 2;
+
+            Goal.Items.AddRange(new string[] { "Набор массы", "Похудение", "Поддержание" });
+            Goal.SelectedIndex = 0;
+
+            CreateDatabase();
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CreateDatabase()
+        {
+            try
+            {
+                using (var conn = new SQLiteConnection("Data Source=CalorieHistory.db;Version=3;"))
+                {
+                    conn.Open();
+                    string sql = @"CREATE TABLE IF NOT EXISTS History (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Date TEXT,
+                Gender TEXT,
+                Age INTEGER,
+                Height REAL,
+                Weight REAL,
+                Activity TEXT,
+                Goal TEXT,
+                Calories REAL,
+                Protein REAL,
+                Fats REAL,
+                Carbs REAL)";
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                        cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка БД: " + ex.Message);
+            }
+        }
+        private double GetCalories()
+        {
+            double age = (double)Age.Value;
+            double height = (double)Height.Value;
+            double weight = (double)Weight.Value;
+            bool isMale = Gender.SelectedIndex == 0;
+
+            // Формула Миффлина-Сан Жеора (базовый метаболизм)      
+            double bmr = isMale
+                ? 10 * weight + 6.25 * height - 5 * age + 5
+                : 10 * weight + 6.25 * height - 5 * age - 161;
+
+            double activity = 1.55;
+            switch (Activity.SelectedIndex)
+            {
+                case 0: activity = 1.2; break;
+                case 1: activity = 1.375; break;
+                case 2: activity = 1.55; break;
+                case 3: activity = 1.725; break;
+                case 4: activity = 1.9; break;
+            }
+
+            double goal = 1.0;
+            switch (Goal.SelectedIndex)
+            {
+                case 0: goal = 1.15; break;
+                case 1: goal = 0.85; break;
+                case 2: goal = 1.0; break;
+            }
+
+            return bmr * activity * goal;
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            double calories = GetCalories();
+            double weight = (double)Weight.Value;
+            var bzhu = GetBZHU(calories, weight);
+
+            Result.Clear();
+            Result.AppendText($"Суточная норма калорий: {calories:F0} ккал\n\n");
+            Result.AppendText($"Белки: {bzhu.protein:F1} г\n");
+            Result.AppendText($"Жиры: {bzhu.fats:F1} г\n");
+            Result.AppendText($"Углеводы: {bzhu.carbs:F1} г\n");
+        }
+
+        private void SaveTxt_Click(object sender, EventArgs e)
+        {
+            if (Result.Text == "")
+            {
+                MessageBox.Show("Сначала нажмите «Рассчитать»");
+                return;
+            }
+
+            double calories = GetCalories();
+            double weight = (double)Weight.Value;
+            var bzhu = GetBZHU(calories, weight);
+
+            string line = $"{DateTime.Now:dd.MM.yyyy HH:mm:ss} | {Gender.Text} | {Age.Value} лет | {Height.Value} см | {Weight.Value} кг | {Activity.Text} | {Goal.Text} | {calories:F0} ккал | {bzhu.protein:F1}г б | {bzhu.fats:F1}г ж | {bzhu.carbs:F1}г у";
+            File.AppendAllText("CalculationHistory.txt", line + Environment.NewLine);
+            MessageBox.Show("Сохранено в CalculationHistory.txt");
+        }
+
+        private void SaveDb_Click(object sender, EventArgs e)
+        {
+            if (Result.Text == "")
+            {
+                MessageBox.Show("Сначала нажмите «Рассчитать»");
+                return;
+            }
+
+            double calories = GetCalories();
+            double weight = (double)Weight.Value;
+            var bzhu = GetBZHU(calories, weight);
+
+            using (var conn = new SQLiteConnection("Data Source=CalorieHistory.db;Version=3;"))
+            {
+                conn.Open();
+                string sql = "INSERT INTO History (Date, Gender, Age, Height, Weight, Activity, Goal, Calories, Protein, Fats, Carbs) VALUES (@date, @gender, @age, @height, @weight, @activity, @goal, @cal, @prot, @fat, @carb)";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@date", DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss"));
+                    cmd.Parameters.AddWithValue("@gender", Gender.Text);
+                    cmd.Parameters.AddWithValue("@age", (int)Age.Value);
+                    cmd.Parameters.AddWithValue("@height", (double)Height.Value);
+                    cmd.Parameters.AddWithValue("@weight", weight);
+                    cmd.Parameters.AddWithValue("@activity", Activity.Text);
+                    cmd.Parameters.AddWithValue("@goal", Goal.Text);
+                    cmd.Parameters.AddWithValue("@cal", calories);
+                    cmd.Parameters.AddWithValue("@prot", bzhu.protein);
+                    cmd.Parameters.AddWithValue("@fat", bzhu.fats);
+                    cmd.Parameters.AddWithValue("@carb", bzhu.carbs);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            MessageBox.Show("Сохранено в базу данных");
+        }
+
+        private void ShowHistory_Click(object sender, EventArgs e)
+        {
+            Result.Clear();
+            using (var conn = new SQLiteConnection("Data Source=CalorieHistory.db;Version=3;"))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM History ORDER BY Id DESC";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Result.AppendText($"--- Запись #{reader["Id"]} ---\n");
+                        Result.AppendText($"Дата: {reader["Date"]}\n");
+                        Result.AppendText($"Цель: {reader["Goal"]}\n");
+                        Result.AppendText($"Калории: {Convert.ToDouble(reader["Calories"]):F0} ккал\n");
+                        Result.AppendText($"Белки: {reader["Protein"]} г, Жиры: {reader["Fats"]} г, Углеводы: {reader["Carbs"]} г\n\n");
+                    }
+                }
+            }
+        }
+        private (double protein, double fats, double carbs) GetBZHU(double calories, double weight)
+        {
+            double proteinPerKg = 1.8;
+            switch (Goal.SelectedIndex)
+            {
+                case 0: proteinPerKg = 2.0; break;
+                case 1: proteinPerKg = 1.6; break;
+                case 2: proteinPerKg = 1.8; break;
+            }
+
+            double protein = weight * proteinPerKg;
+            double proteinCalories = protein * 4;
+
+            double fatPercentage = 0.25;
+            switch (Goal.SelectedIndex)
+            {
+                case 0: fatPercentage = 0.28; break;  
+                case 1: fatPercentage = 0.22; break; 
+                case 2: fatPercentage = 0.25; break; 
+            }
+            double fatCalories = calories * fatPercentage;
+            double fats = fatCalories / 9; 
+
+           
+            double carbsCalories = calories - proteinCalories - fatCalories;
+            double carbs = carbsCalories / 4;  
+
+            if (carbs < 0) carbs = 0;
+
+            return (protein, fats, carbs);
+        }
+
+        private void Gender_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
